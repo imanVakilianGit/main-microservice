143e0c195efc9eb136cde6cf975934abaf134a76
imanvakilian <imanvakiliangitu@gmail.com>
2025-02-27 14:20:53 +0330

    (feat) configure app as microservice app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { INestMicroservice } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { TcpOptions, Transport } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+async function bootstrap(): Promise<void> {
+    const host: string = process.env.HOST;
+    const port: number = Number(process.env.PORT);
+
+    const app: INestMicroservice = await NestFactory.createMicroservice<TcpOptions>(AppModule, {
+        transport: Transport.TCP,
+        options: {
+            host,
+            port,
+        },
+    });
+
+    await app.listen();
+    console.log(`app running on host: ${host} and port: ${port}`);
 }
 bootstrap();
