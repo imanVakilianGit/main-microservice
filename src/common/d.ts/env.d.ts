0edba07c6dea326adeabffa344514798d9e42afa
imanvakilian <imanvakiliangitu@gmail.com>
2025-03-04 17:08:10 +0330

    (feat) add new env values

--- src/common/d.ts/env.d.ts
+++ src/common/d.ts/env.d.ts
@@ -2,5 +2,10 @@ declare namespace NodeJS {
     export interface ProcessEnv {
         HOST: string;
         PORT: string;
+        DATABASE_HOST: string;
+        DATABASE_PORT: number;
+        DATABASE_PASSWORD: string;
+        DATABASE_USERNAME: string;
+        DATABASE_NAME: string;
     }
 }
