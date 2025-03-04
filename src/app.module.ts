9a70f88a2749e272f487d1f0250875bd00bc09c5
imanvakilian <imanvakiliangitu@gmail.com>
2025-03-04 17:11:22 +0330

    (feat) configure typeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { staticModules } from './config/static.module';
+import { dynamicModules } from './config/dynamic.module';
 
 @Module({
-    imports: [...staticModules],
+    imports: [...staticModules, ...dynamicModules],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/config/dynamic.module.ts
+++ src/config/dynamic.module.ts
@@ -1,1 +1,25 @@
-export const dynamicModules = [];
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const dynamicModules = [
+    ConfigModule.forRoot({
+        isGlobal: true,
+        // envFilePath: `${process.cwd()}/.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+        useFactory: (configService: ConfigService) => {
+            return {
+                type: 'postgres',
+                database: configService.getOrThrow<string>('DATABASE_NAME'),
+                host: configService.getOrThrow<string>('DATABASE_HOST'),
+                port: configService.getOrThrow<number>('DATABASE_PORT'),
+                password: configService.getOrThrow<string>('DATABASE_PASSWORD'),
+                username: configService.getOrThrow<string>('DATABASE_USERNAME'),
+                entities: [],
+                migrations: [],
+                synchronize: false,
+            };
+        },
+        inject: [ConfigService],
+    }),
+];
